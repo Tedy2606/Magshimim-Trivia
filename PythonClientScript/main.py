4e476bc313ca0ec62df59e6fef707f33949ee315
Eyal Gal <170107eyalgal@gmail.com>
2024-03-31 20:44:40 +0300

    Added an exception for socket error and a cool animation until trying to connect again ;)

--- PythonClientScript/main.py
+++ PythonClientScript/main.py
@@ -1,4 +1,6 @@
+import os
 import socket
+from time import sleep
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 56812  # The port used by the server
@@ -6,18 +8,37 @@ EXIT = 'EXIT'
 
 
 def main():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_sock:
-        server_sock.connect((HOST, PORT))  # connect to the server
+    # connect to server
+    tries_left = 5  # tries until
+    print('Connecting to server')
 
-        while message != EXIT:
-            # send message to the server
-            message = input('Enter message: ')
-            server_sock.sendall(message.encode())
+    while tries_left:  # when connection doesn't work try again
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_sock:
+                server_sock.connect((HOST, PORT))  # connect to the server
+                print('Connection has been made with the server')
 
-            # get response from the serv er
-            response = server_sock.recv(1024)
-            print(response.decode())
-            
+                while message != EXIT:
+                    # send message to the server
+                    message = input('Enter message: ')
+                    server_sock.sendall(message.encode())
+
+                    # get response from the serv er
+                    response = server_sock.recv(1024)
+                    print(response.decode())
+            return  # end program
+        except socket.error:
+            print('Connection with server has Failed\nTrying again ' + str(tries_left), end='')
+            tries_left -= 1
+
+            # cool waiting animation
+            for i in range(3):
+                sleep(1)
+                print('.', end='')
+            print()  # go down a line
+
+    print('Terminating program')
+
 
 if __name__ == '__main__':
     main()
