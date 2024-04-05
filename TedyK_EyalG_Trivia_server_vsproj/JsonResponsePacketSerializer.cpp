c131b7aacc282ccaff4829e8c3e249781436b98d
Tedy koudritsky <tedkoud@gmail.com>
2024-04-05 21:30:34 +0300

    realised I did everything wrong removed previus additions and made small additions to the seri functions

--- TedyK_EyalG_Trivia_server_vsproj/JsonResponsePacketSerializer.cpp
+++ TedyK_EyalG_Trivia_server_vsproj/JsonResponsePacketSerializer.cpp
@@ -1,52 +1,34 @@
 #include "JsonResponsePacketSerializer.h"
-
+#include "json.hpp"
+using json = nlohmann::json;
 buffer JsonResponsePacketSerializer::serializeResponse(ErrorResponse response)
 {
-    //not really sure if this is correct or not
-
-      // Make the buffer
-    std::vector<unsigned char> buf;
-
-    // Convert the error string to a char arr
-    const char* errorStr = response.err.c_str();
-    size_t strLength = response.err.length();
-
-
-    //convert each char into unsigned and push it 
-    for (size_t i = 0; i < strLength; ++i) {
-        unsigned char byte = errorStr[i];
-        buf.push_back(byte);
-    }
-    return buf;
+   
+    return buffer();
 }
 
 buffer JsonResponsePacketSerializer::serializeResponse(LoginResponse response)
 {
+    //make the data json 
+    json data = json::parse(R"(
+  {
+    "status": 1,
+  }
+)");
     //make the buffer
     std::vector<unsigned char> buf;
 
-    //make an array so we can copy the bytes
-    unsigned char statusBytes[sizeof(int)];
-    memcpy(statusBytes, (char*)&response.status, sizeof(int));
+   
+    //should probably get the jso into the buffer usign memcpy (something like this)
+    //memcpy(statusBytes, (char*)&response.status, sizeof(int));
 
     //insert bytes into the vector
-    buf.insert(buf.end(), statusBytes, statusBytes + sizeof(int));
+    //buf.insert(buf.end(), statusBytes, statusBytes + sizeof(int));
     return buf;
 }
 
 buffer JsonResponsePacketSerializer::serializeResponse(SignupResponse response)
 {
-    //make the buffer
-    std::vector<unsigned char> buf;
-
-    //make an array so we can copy the bytes
-    unsigned char statusBytes[sizeof(int)];
-    memcpy(statusBytes, (char*)&response.status, sizeof(int));
-
-    //insert bytes into the vector
-    buf.insert(buf.end(), statusBytes, statusBytes + sizeof(int));
-
-
-
-    return buf;
+    
+    return buffer();
 }
