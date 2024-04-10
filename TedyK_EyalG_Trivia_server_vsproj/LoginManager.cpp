106107b6562018b72f9718a33fadf00310175bc8
Tedy koudritsky <tedkoud@gmail.com>
2024-04-10 20:48:25 +0300

    finished the logout function in login manager

--- TedyK_EyalG_Trivia_server_vsproj/LoginManager.cpp
+++ TedyK_EyalG_Trivia_server_vsproj/LoginManager.cpp
@@ -24,4 +24,20 @@ void LoginManager::login(string name, string password)
 
 void LoginManager::logout(string name)
 {
+	if (this->m_database->doesUserExists(name))
+	{
+		//do an error or something
+		return;
+	}
+
+	//finds the position of the user in the vector 
+	int count = 0;
+	for (; count < this->m_loggedUsers.size(); count++)
+	{
+		if (this->m_loggedUsers[count].GetUserName() == name)
+			break;
+
+	}
+	//removes from the vector
+	this->m_loggedUsers.erase(this->m_loggedUsers.begin() + count);
 }
