eea70835a450dda9793cf9207364ebcb84beec37
Tedy koudritsky <tedkoud@gmail.com>
2024-04-21 23:01:10 +0300

    finished most of the functions of room, ( get data, remove, add)

---
+++ TedyK_EyalG_Trivia_server_vsproj/Room.cpp
@@ -0,0 +1,29 @@
+#include "Room.h"
+
+Room::Room(RoomData data)
+{
+	this->m_metadata = data;
+	m_users = std::vector<LoggedUser>();
+}
+
+RoomData Room::getData() const
+{
+	return this->m_metadata;
+}
+
+void Room::addUser(LoggedUser user)
+{
+	this->m_users.push_back(user);
+}
+
+void Room::removeUser(LoggedUser user)
+{
+	//go over all of the rooms andd erase the given one 
+	for (auto it = this->m_users.begin(); it != this->m_users.end(); it++)
+	{
+		if (it->GetUserName() == user.GetUserName()) {
+			this->m_users.erase(it);
+			break;
+		}
+	}
+}
