3196e0d71e315260fa94fc17acc80ff910617f6e
Tedy koudritsky <tedkoud@gmail.com>
2024-04-21 22:56:23 +0300

    added neccecasry h files

---
+++ TedyK_EyalG_Trivia_server_vsproj/RoomManager.h
@@ -0,0 +1,48 @@
+#pragma once
+#include "Room.h"
+#include <map>
+using std::string;
+class RoomManager
+{
+public:
+
+	/***
+	* function creates a room
+	* @param RoomData - the data of the room
+	* @param LoggedUser - the user that makes the room
+	* @return none
+	**/
+	void createRoom(LoggedUser user, RoomData data);
+
+
+	/***
+	* function deletes a room
+	* @param int ID - the id of the wanted room 
+	* @return none
+	**/
+	void deleteRoom(int ID);
+
+	/***
+	* function return the state that the room is in 
+	* @param int ID - the id of the wanted room 
+	* @return int - the state
+	**/
+	unsigned int getRoomState(int ID);
+
+	/***
+	* function returns all rooms
+	* @return vector full of all of the rooms data
+	**/
+	std::vector<RoomData> getRooms();
+
+	/***
+	* function returns a room based on its id
+	* @param int ID - the id of the room
+	* @return Room - the requested room 
+	**/
+	Room& getRoom(int ID);
+
+private:
+	std::map<int, Room> m_rooms;
+};
+
