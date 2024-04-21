55aad07eaac2b8a6b30fb8655fcb6bf79c1c7d59
Tedy koudritsky <tedkoud@gmail.com>
2024-04-21 23:26:40 +0300

    finished the create and join room requests

--- TedyK_EyalG_Trivia_server_vsproj/JsonResponsePacketDeserializer.cpp
+++ TedyK_EyalG_Trivia_server_vsproj/JsonResponsePacketDeserializer.cpp
@@ -4,6 +4,30 @@
 #include <sstream>
 using json = nlohmann::json;
 #define HEADER_END 5
+CreateRoomRequest JsonResponsePacketDeserializer::desirializeCreateRoomRequest(std::vector<unsigned char> buffer)
+{
+    //make the json
+    json data_as_json = bufferToJson(buffer);
+    //make the request
+    CreateRoomRequest request;
+    request.answerTimeout = data_as_json["answerTimeout"];
+    request.maxUsers = data_as_json["maxUsers"];
+    request.questionCount = data_as_json["questionCount"];
+    request.roomName = data_as_json["roomName"];
+    return request;
+}
+
+JoinRoomRequest JsonResponsePacketDeserializer::desirializeJoinRoomRequest(std::vector<unsigned char> buffer)
+{
+    json data_as_json = bufferToJson(buffer);
+
+    //make the request
+    JoinRoomRequest request;
+    request.roomID = data_as_json["roomID"];
+
+    return request;
+}
+
 LoginRequest JsonResponsePacketDeserializer::desirializeLoginRequest(std::vector<unsigned char> buffer)
 {
 
