b41e5c4bce4eec84f8266ece7db972772681dd44
Eyal Gal <170107eyalgal@gmail.com>
2024-04-24 10:23:58 +0300

    Implemented getUsersStatistics method in the StatisticsManager class

--- TedyK_EyalG_Trivia_server_vsproj/StatisticsManager.cpp
+++ TedyK_EyalG_Trivia_server_vsproj/StatisticsManager.cpp
@@ -7,5 +7,14 @@ std::vector<std::string> StatisticsManager::getHighScore()
 
 std::vector<std::string> StatisticsManager::getUserStatistics(std::string username)
 {
-    return std::vector<std::string>();
+    std::vector<std::string> userStatistics;
+
+    // push all the statistics to the vector
+    userStatistics.push_back(std::to_string(this->m_database.getPlayerScore(username));
+    userStatistics.push_back(std::to_string(this->m_database.getNumOfPlayerGames(username));
+    userStatistics.push_back(std::to_string(this->m_database.getNumOfTotalAnswers(username));
+    userStatistics.push_back(std::to_string(this->m_database.getNumOfCorrectAnswers(username));
+    userStatistics.push_back(std::to_string(this->m_database.getPlayerAverageAnswerTime(username));
+
+    return userStatistics;
 }
