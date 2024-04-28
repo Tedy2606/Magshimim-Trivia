9e4b92ddbe3d9411dc94255b80b3f7ce8603f9e3
Tedy koudritsky <tedkoud@gmail.com>
2024-04-28 11:43:42 +0300

    changed the constructor of menu request handler

--- TedyK_EyalG_Trivia_server_vsproj/MenuRequestHandler.cpp
+++ TedyK_EyalG_Trivia_server_vsproj/MenuRequestHandler.cpp
@@ -1,9 +1,27 @@
 #include "MenuRequestHandler.h"
+#include "RequestHandlerFactory.h"
+#include "JsonResponsePacketSerializer.h"
+#include "JsonResponsePacketDeserializer.h"
 
-MenuRequestHandler::MenuRequestHandler(RequestHandlerFactory& handlerFactory)
-    : m_handlerFactory(handlerFactory)
+#define LOGOUT_MSG_CODE 104
+
+#define JOIN_ROOM_MSG_CODE 105
+#define CREATE_ROOM_MSG_CODE 106
+#define GET_STATS_MSG_CODE 107
+#define GET_HIGH_SCORE_MSG_CODE 108
+
+
+#define GET_PLAYERS_IN_ROOM_MSG_CODE 109
+#define GET_ROOMS_MSG_CODE 110
+
+
+#define OK_RESPONSE 1
+
+MenuRequestHandler::MenuRequestHandler(RequestHandlerFactory& handlerFactory, LoggedUser user)
+    : m_handlerFactory(handlerFactory), m_user(user)
 {
 }
+
 
 bool MenuRequestHandler::isRequestRelevant(RequestInfo info)
 {
