caa4e38898e6bcb8b632d2671f6a036f14d9edc4
Eyal Gal <170107eyalgal@gmail.com>
2024-05-13 22:15:59 +0300

    Adding a PlayerStatistics page

--- TriviaClient/PlayerStatistics.xaml.cs
+++ TriviaClient/PlayerStatistics.xaml.cs
@@ -16,15 +16,13 @@ using System.Windows.Shapes;
 namespace TriviaClient
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for PlayerStatistics.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class PlayerStatistics : Page
     {
-        public MainWindow()
+        public PlayerStatistics()
         {
             InitializeComponent();
-            //img.Source = new ImageSource("/images/title.jpg");
-            frame.Navigate(new Player());
         }
     }
 }
