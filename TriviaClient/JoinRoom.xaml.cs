d3cf8c0eac6f77655aa815b60548a14ffd9769cb
Eyal Gal <170107eyalgal@gmail.com>
2024-06-12 04:45:59 +0300

    fixed room names showing with ',' at the start of their name

--- TriviaClient/JoinRoom.xaml.cs
+++ TriviaClient/JoinRoom.xaml.cs
@@ -106,7 +106,7 @@ namespace TriviaClient
             }
             else
             {
-                Regex regex = new Regex(@"\b[^:]+:\d+:\d+\b");
+                Regex regex = new Regex(@"\b[^:,]+:\d+:\d+\b");
                 
                 // get the button name:id:isActive string
                 string rooms = jsonObject.Value<string>("rooms");
